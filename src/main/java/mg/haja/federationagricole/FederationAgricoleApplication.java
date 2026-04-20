f2d3fff88445aaf68494a58799c031f0aa37d5bc
haja171106 <hei.haja.2@gmail.com>
2026-04-20 10:41:07 +0300

    initial commit

---
+++ src/main/java/mg/haja/federationagricole/FederationAgricoleApplication.java
@@ -0,0 +1,13 @@
+package mg.haja.federationagricole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FederationAgricoleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FederationAgricoleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mg/haja/federationagricole/FederationAgricoleApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.haja.federationagricole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FederationAgricoleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
