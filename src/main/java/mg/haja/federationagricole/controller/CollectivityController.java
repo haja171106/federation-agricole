5206170ff8ded92559fb4d84cc6e77c84a75f7b5
haja171106 <hei.haja.2@gmail.com>
2026-04-22 09:18:51 +0300

    feat: modification in specification

--- src/main/java/mg/haja/federationagricole/controller/CollectivityController.java
+++ src/main/java/mg/haja/federationagricole/controller/CollectivityController.java
@@ -1,8 +1,10 @@
 package mg.haja.federationagricole.controller;
 
+import mg.haja.federationagricole.DTO.AssignIdentificationRequest;
 import mg.haja.federationagricole.DTO.CreateCollectivityRequest;
 import mg.haja.federationagricole.Entity.Collectivity;
 import mg.haja.federationagricole.service.CollectivityService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,5 +20,14 @@ public class CollectivityController {
     @PostMapping
     public Collectivity createCollectivity(@RequestBody CreateCollectivityRequest request) {
         return service.createCollectivity(request);
+    }
+
+    @PostMapping("/{id}/identification")
+    public ResponseEntity<Collectivity> assignIdentification(
+            @PathVariable int id,
+            @RequestBody AssignIdentificationRequest request
+    ) {
+        Collectivity result = service.assignIdentification(id, request);
+        return ResponseEntity.ok(result);
     }
 }
--- src/main/java/mg/haja/federationagricole/service/CollectivityService.java
+++ src/main/java/mg/haja/federationagricole/service/CollectivityService.java
@@ -1,5 +1,6 @@
 package mg.haja.federationagricole.service;
 
+import mg.haja.federationagricole.DTO.AssignIdentificationRequest;
 import mg.haja.federationagricole.DTO.CreateCollectivityRequest;
 import mg.haja.federationagricole.Entity.Collectivity;
 import mg.haja.federationagricole.repository.CollectivityRepository;
@@ -16,27 +17,46 @@ public class CollectivityService {
 
     public Collectivity createCollectivity(CreateCollectivityRequest request) {
 
-        repository.findByNumber(request.number)
-                .ifPresent(c -> {
-                    throw new RuntimeException("Collectivity number already exists");
-                });
-
         if (!request.federationApproval) {
             throw new RuntimeException("Federation approval required");
         }
 
-        if (request.members.size() < 10) {
+        if (request.members == null || request.members.size() < 10) {
             throw new RuntimeException("At least 10 members required");
         }
 
         Collectivity c = new Collectivity();
-        c.setName(request.name);
-        c.setNumber(request.number);
+
         c.setCity(request.city);
         c.setAgriculturalSpecialty(request.agriculturalSpecialty);
         c.setCreationDate(request.creationDate);
         c.setOpeningAuthorization(true);
 
         return repository.save(c);
+    }
+
+    public Collectivity assignIdentification(int id, AssignIdentificationRequest request) {
+
+        Collectivity collectivity = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Collectivity not found"));
+
+        if (collectivity.getName() != null || collectivity.getNumber() != null) {
+            throw new RuntimeException("Identification already assigned");
+        }
+
+        repository.findByName(request.getName())
+                .ifPresent(c -> {
+                    throw new RuntimeException("Name already exists");
+                });
+
+        repository.findByNumber(request.getNumber())
+                .ifPresent(c -> {
+                    throw new RuntimeException("Number already exists");
+                });
+
+        collectivity.setName(request.getName());
+        collectivity.setNumber(request.getNumber());
+
+        return repository.save(collectivity);
     }
 }
